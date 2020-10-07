c0940a109e121b2185faf69ed433e5ce7e1880a4
Nejc Jelovčan <nejc.jelovcan@gmail.com>
2020-10-07 19:05:06 +0200

    typeof typo - need more typeguards!

--- packages/wlq-api-aws/src/lambdas/issueQuestionToken.ts
+++ packages/wlq-api-aws/src/lambdas/issueQuestionToken.ts
@@ -5,7 +5,10 @@ const DbProps = getDatabaseProps()
 
 export const handler = async (event: { [key: string]: unknown }) => {
   console.log('Issue question token', event)
-  if (typeof event.roomId === 'string' && event.questionToken === 'string') {
+  if (
+    typeof event.roomId === 'string' &&
+    typeof event.questionToken === 'string'
+  ) {
     await setRoomQuestionTokenCallback(DbProps)(
       event.roomId,
       event.questionToken,
